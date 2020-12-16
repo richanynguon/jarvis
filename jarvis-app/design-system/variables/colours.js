34ee4813e225e526ebd855a22b7f9b001e0318b7
Richany <alina.nguon@gmail.com>
2020-12-16 12:34:41 -0500

    design variables

---
+++ jarvis-app/design-system/variables/colours.js
@@ -0,0 +1,12 @@
+  
+export const PRIMARY_POP_COLOR = '#44D6E1';
+export const SECONDARY_POP_COLOR = '#B4FAFF';
+export const LIGHT_NEUTRAL_COLOR = '#F6F1F0';
+export const DARK_NEUTRAL_COLOR = '#444140';
+
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
+
+export const SUCCESS_COLOR = '#61994c';
+export const WARNING_COLOR = '#de8914';
+export const DANGER_COLOR = '#f44336';
---
+++ jarvis-app/design-system/variables/global.js
@@ -0,0 +1,4 @@
+// in styled components will be
+// @media all and (min-width: ${mediaBreak})
+export const phoneMediaBreak = "fill";
+export const desktopMediaBreak = "fill";
